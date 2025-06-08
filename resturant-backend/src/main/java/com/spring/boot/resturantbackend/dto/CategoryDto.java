8b0c0e505583631ac4c536a1aebadb81ee869bb8
taherElzoghby27 <tataamen678@gmail.com>
2025-06-08 19:28:51 +0300

    add swagger

--- resturant-backend/src/main/java/com/spring/boot/resturantbackend/dto/CategoryDto.java
+++ resturant-backend/src/main/java/com/spring/boot/resturantbackend/dto/CategoryDto.java
@@ -2,6 +2,7 @@ package com.spring.boot.resturantbackend.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.spring.boot.resturantbackend.vm.ProductVm;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -16,14 +17,27 @@ import java.util.List;
 @Setter
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(
+        name = "Category Dto",
+        description = "category dto contains (id,name,logo,flag,products)"
+)
 public class CategoryDto {
     private Long id;
     @NotEmpty(message = "not_empty.name")
     @Size(min = 7, max = 50, message = "size.name")
+    @Schema(
+            name = "name",
+            description = "name for category (string)",
+            example = "taher amin"
+    )
     private String name;
     @NotEmpty(message = "not_empty.logo")
     private String logo;
     @NotEmpty(message = "not_empty.flag")
     private String flag;
+    @Schema(
+            name = "products",
+            description = "list of products (List<ProductVm>)"
+    )
     private List<ProductVm> products;
 }
