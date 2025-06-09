277d0fc7c09be8b5324ba16b88667bc51501a103
taherElzoghby27 <tataamen678@gmail.com>
2025-06-09 15:30:26 +0300

    add auth filter

--- resturant-backend/src/main/java/com/spring/boot/resturantbackend/config/security/SecurityConfig.java
+++ resturant-backend/src/main/java/com/spring/boot/resturantbackend/config/security/SecurityConfig.java
@@ -1,7 +1,19 @@
 package com.spring.boot.resturantbackend.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                authorizeRequests -> authorizeRequests
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+        );
+        return http.build();
+    }
 }
--- resturant-backend/src/main/java/com/spring/boot/resturantbackend/controllers/AuthController.java
+++ resturant-backend/src/main/java/com/spring/boot/resturantbackend/controllers/AuthController.java
@@ -1,4 +1,8 @@
 package com.spring.boot.resturantbackend.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/auth")
+@RestController
 public class AuthController {
 }
