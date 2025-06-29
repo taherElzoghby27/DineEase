6548f669f58fffdc680932211cb074cdc3b00d1d
taherElzoghby27 <tataamen678@gmail.com>
2025-06-30 00:55:15 +0300

    remove categories when in card details

--- front-end-903/src/app/app.component.ts
+++ front-end-903/src/app/app.component.ts
@@ -1,16 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../service/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(private authService: AuthService) {
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {
   }
+
+  isCardDetails = false;
 
   isLogin(): boolean {
     return this.authService.isLogin();
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      const url = this.router.url;
+      if (url.includes('/cardDetails')) {
+        this.isCardDetails = true;
+      } else {
+        this.isCardDetails = false;
+      }
+    });
   }
 }
