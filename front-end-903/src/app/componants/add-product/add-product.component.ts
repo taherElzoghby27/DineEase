0028975f3a1fb19ff29dbbd2e00fe18ed0a7e524
taherElzoghby27 <tataamen678@gmail.com>
2025-07-08 02:19:21 +0300

    added product

--- front-end-903/src/app/componants/add-product/add-product.component.ts
+++ front-end-903/src/app/componants/add-product/add-product.component.ts
@@ -1,4 +1,10 @@
 import {Component, OnInit} from '@angular/core';
+import {ProductService} from '../../../service/product.service';
+import {ProductRequest} from '../../../model/product-request';
+import {Category} from '../../../model/category';
+import {CategoryService} from '../../../service/category.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-add-product',
@@ -6,36 +12,69 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
-  product = {
-    name: '',
-    description: '',
-    price: null,
-    image: '',
-    preparationTime: null,
-    productCode: ''
-  };
+  name = '';
+  description = '';
+  price = 0.0;
+  image = '';
+  categoryId?: number;
+  categories: Category[] = [];
+  errorAr = '';
+  errorEn = '';
+  errorBackend = false;
 
-  constructor() {
+  constructor(private productService: ProductService,
+              private categoryService: CategoryService,
+              private activatedRoute: ActivatedRoute,
+              private snackBar: MatSnackBar,
+              private router: Router) {
   }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(params => this.getAllCategories());
   }
 
 
   isFormValid(): boolean {
-    return (
-      !!this.product.name?.trim() &&
-      !!this.product.description?.trim() &&
-      this.product.price !== null
-    );
+    return !(this.name === '' || this.description === '' || this.price <= 0 || (this.categoryId === undefined));
   }
 
   addProduct(): void {
-    if (this.isFormValid()) {
-      console.log('Product to be added:', this.product);
-      // Send product to API or service here
-    } else {
-      console.warn('Required fields are missing');
+    if (!this.isFormValid()) {
+      return;
     }
+    const productRequest = new ProductRequest({
+      name: this.name,
+      imagePath: this.image,
+      description: this.description,
+      price: this.price,
+      categoryId: this.categoryId,
+    });
+
+    this.productService.createProduct(productRequest).subscribe(
+      product => {
+        this.errorBackend = false;
+        this.snackBar.open('Product Added Successfully', 'Close', {
+          duration: 3000, // milliseconds
+          verticalPosition: 'top', // or 'bottom'
+          panelClass: ['snackbar-success']
+        });
+        this.router.navigateByUrl('products');
+      },
+      errors => {
+        this.errorBackend = true;
+        this.errorAr = errors.error.bundleMessage.message_ar;
+        this.errorEn = errors.error.bundleMessage.message_en;
+      }
+    );
+  }
+
+  getAllCategories(): void {
+    this.categoryService.getAllCategories().subscribe(
+      response => {
+        this.categories = response;
+      },
+      errors => {
+      }
+    );
   }
 }
--- front-end-903/src/model/product-request.ts
+++ front-end-903/src/model/product-request.ts
@@ -3,7 +3,7 @@ import {ProductDetailsResponse} from './product-details-response';
 export class ProductRequest {
   id?: number;
   name: string;
-  imagePath: string;
+  imagePath?: string;
   description: string;
   price: number;
   categoryId?: number;
